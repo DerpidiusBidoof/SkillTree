603898a1db156deac242c685e9091288ee59d564
Derpidius <derpidiusbidoof@gmail.com>
2023-10-24 13:03:34 -0500

    Skill code update ui method working

--- SkillTree/Assets/Code/Skill.cs
+++ SkillTree/Assets/Code/Skill.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using static SkillTree;
 
 public class Skill : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Every skill will have an id to track which upgrade it is.
+    public int id;
+
+    public TMP_Text SkillTitleText;
+    public TMP_Text SkillDescriptionText;
+
+    //For marking which skills are connected to each other.
+    public int[] ConnectedSkills;
+
+    public 
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    public void UpdateUI()
+    {
+        SkillTitleText.text = $"{skillTree.SkillLevels[id]}/{skillTree.SkillCaps[id]}\n{ SkillTree.skillTree.SkillNames[id]}";
+        SkillDescriptionText.text = $"{skillTree.SkillDescriptions[id]}\nCost: {skillTree.SkillPoints}/1 SP";
+
+        GetComponent<Image>().color = skillTree.SkillLevels[id] >= skillTree.SkillCaps[id] ? Color.yellow
+            : skillTree.SkillPoints >= 1 ? Color.green : Color.white;
+    }
+
+    public void BuySkill()
+    {
+
     }
 }
